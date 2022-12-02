d4029c146109d8c3fe9d5a781f56ba55c0ea20db
aldinkapetanovic@gmail.com <aldinkapetanovic@gmail.com>
2022-12-02 21:51:44 +0100

    blah

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
-# variable "AWS_ACCESS_KEY_ID" {
-#   default = null
-# }
+variable "AWS_ACCESS_KEY_ID" {
+  default = null
+}
 
-# variable "AWS_SECRET_ACCESS_KEY" {
-#   default = null
-# }
+variable "AWS_SECRET_ACCESS_KEY" {
+  default = null
+}
