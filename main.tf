cbec5ad84b11633fd27fd336c1674d992cd4422c
aldinkapetanovic@gmail.com <aldinkapetanovic@gmail.com>
2022-12-02 21:23:15 +0100

    provider credentials

--- main.tf
+++ main.tf
@@ -10,7 +10,9 @@ terraform {
 }
 
 provider "aws" {
-  region = "eu-west-1"
+  region     = "eu-west-1"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 resource "aws_instance" "app_server" {
